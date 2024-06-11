fa8cbc45e916cdf30234b1c220f518f6b60fe771
hyensukim <crazymin17@naver.com>
2024-06-11 11:34:56 +0900

    ExceptionHandling-@ExceptionHandler

--- src/main/java/org/study/spring/rest/exceptionhandling/AnotherController.java
+++ src/main/java/org/study/spring/rest/exceptionhandling/AnotherController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AnotherController{
+public class AnotherController extends ExceptionHandleController{
 
     @GetMapping("api-call/error")
     public void method1(){
